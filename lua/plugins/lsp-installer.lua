ac52ad04b37b8218eb63e073742cb046a6bcb1e6
rory <ranesbitt@live.co.uk>
2022-01-02 20:14:55 +0000

    Removed the vim is not a global variable error and added some extra setup

--- lua/plugins/lsp-installer.lua
+++ lua/plugins/lsp-installer.lua
@@ -6,13 +6,27 @@ end
 -- Register a handler that will be called for all installed servers.
 -- Alternatively, you may also register handlers on specific server instances instead (see example below).
 lsp_installer.on_server_ready(function(server)
-    local opts = {}
-
-    -- if server.name == "tsserver" then
-    --     opts.root_dir = function() ... end
-    -- end
-
-    -- This setup() function is exactly the same as lspconfig's setup function.
-    -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-    server:setup(opts)
+  local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  local opts = {capabilities = capabilities}
+  if server.name == 'sumneko_lua' then
+    opts = vim.tbl_deep_extend('force', {
+      settings = {
+        Lua = {
+          runtime = {
+            path = vim.split(package.path, ';')
+          },
+          diagnostics = {
+            globals = {'vim'}
+          },
+          workspace = {
+            library = vim.api.nvim_get_runtime_file('', true),
+            checkThirdParty = false
+          }
+        }
+      }
+    }, opts)
+  end
+  -- This setup() function is exactly the same as lspconfig's setup function.
+  -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+  server:setup(opts)
 end)
