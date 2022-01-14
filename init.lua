023881c3b97231884dcf6e0dbb4930f3703f2255
rory <ranesbitt@live.co.uk>
2022-01-14 15:27:46 +0000

    Moved basic-init to main init

--- init.lua
+++ init.lua
@@ -1,4 +1,8 @@
-require("basic-init")
+vim.g.mapleader = " "
+vim.o.termguicolors = true
+--vim.o.t_Co = '256'
+--vim.bo.t_Co = '256'
+
 require("packer-init")
 require("plugins.shortConfs")
 require("options")
