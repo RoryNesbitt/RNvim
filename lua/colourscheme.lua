87b9018b7245bd49cded1a4085c87b98a66bbc27
rory <ranesbitt@live.co.uk>
2022-02-05 18:32:22 +0000

    Added LineNr to colourscheme

--- lua/colourscheme.lua
+++ lua/colourscheme.lua
@@ -1,4 +1,4 @@
-local colorscheme = "gruvbox"
+local colorscheme = "base16-gruvbox-dark-hard"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
@@ -9,6 +9,7 @@ end
 vim.cmd([[au VimEnter * highlight ColorColumn ctermbg=8]])
 vim.cmd([[au VimEnter * highlight Normal guibg=none]])
 vim.cmd([[au VimEnter * highlight NonText guibg=none]])
+vim.cmd([[au VimEnter * highlight LineNr guibg=none]])
 
 --Marks trailing whitespace as an warning
 vim.cmd([[
