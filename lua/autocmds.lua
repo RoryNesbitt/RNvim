1aa3dec785b002b3b8a8314d0c644cd3bf844c35
rory <ranesbitt@live.co.uk>
2022-02-06 12:09:26 +0000

    Added markdown textwidth

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,13 +1,16 @@
 vim.cmd([[
-    augroup packer-updater
-        autocmd!
-        autocmd BufWritePost packer-init.lua source <afile> | PackerSync
-    augroup end
-]])
+  augroup packer-updater
+    autocmd!
+    autocmd BufWritePost packer-init.lua source <afile> | PackerSync
+  augroup end
 
-vim.cmd([[
-    augroup tmux-reload
-        autocmd!
-        autocmd BufWritePost tmux.conf | !tmux source-file ~/.config/tmux/tmux.conf; tmux display-message "tmux.conf reloaded"
-    augroup end
+  augroup tmux-reload
+    autocmd!
+    autocmd BufWritePost tmux.conf | !tmux source-file ~/.config/tmux/tmux.conf; tmux display-message "tmux.conf reloaded"
+  augroup end
+
+  augroup my_textwidth
+    au!
+    autocmd FileType text,markdown,tex setlocal textwidth=80
+  augroup END
 ]])
