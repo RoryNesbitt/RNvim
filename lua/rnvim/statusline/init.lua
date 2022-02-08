2602cece7b2898ecef3ff5f2f970eb6932620fdc
rory <ranesbitt@live.co.uk>
2022-02-08 17:21:47 +0000

    Stopped it freaking out about lspConfig

--- lua/rnvim/statusline/init.lua
+++ lua/rnvim/statusline/init.lua
@@ -203,56 +203,56 @@ local Ruler = {
     provider = "%7(%l/%3L%):%2c %P",
 }
 
-local Diagnostics = {
-
-    condition = conditions.has_diagnostics,
-
-    static = {
-        error_icon = vim.fn.sign_getdefined("DiagnosticSignError")[1].text,
-        warn_icon = vim.fn.sign_getdefined("DiagnosticSignWarn")[1].text,
-        info_icon = vim.fn.sign_getdefined("DiagnosticSignInfo")[1].text,
-        hint_icon = vim.fn.sign_getdefined("DiagnosticSignHint")[1].text,
-    },
-
-    init = function(self)
-        self.errors = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.ERROR })
-        self.warnings = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.WARN })
-        self.hints = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.HINT })
-        self.info = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.INFO })
-    end,
-
-    {
-        provider = "![",
-    },
-    {
-        provider = function(self)
-            -- 0 is just another output, we can decide to print it or not!
-            return self.errors > 0 and (self.error_icon .. self.errors .. " ")
-        end,
-        hl = { fg = colors.diag.error },
-    },
-    {
-        provider = function(self)
-            return self.warnings > 0 and (self.warn_icon .. self.warnings .. " ")
-        end,
-        hl = { fg = colors.diag.warn },
-    },
-    {
-        provider = function(self)
-            return self.info > 0 and (self.info_icon .. self.info .. " ")
-        end,
-        hl = { fg = colors.diag.info },
-    },
-    {
-        provider = function(self)
-            return self.hints > 0 and (self.hint_icon .. self.hints)
-        end,
-        hl = { fg = colors.diag.hint },
-    },
-    {
-        provider = "]",
-    },
-}
+-- local Diagnostics = {
+-- 
+    -- condition = conditions.has_diagnostics,
+-- 
+    -- static = {
+        -- error_icon = vim.fn.sign_getdefined("DiagnosticSignError")[1].text,
+        -- warn_icon = vim.fn.sign_getdefined("DiagnosticSignWarn")[1].text,
+        -- info_icon = vim.fn.sign_getdefined("DiagnosticSignInfo")[1].text,
+        -- hint_icon = vim.fn.sign_getdefined("DiagnosticSignHint")[1].text,
+    -- },
+-- 
+    -- init = function(self)
+        -- self.errors = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.ERROR })
+        -- self.warnings = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.WARN })
+        -- self.hints = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.HINT })
+        -- self.info = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.INFO })
+    -- end,
+-- 
+    -- {
+        -- provider = "![",
+    -- },
+    -- {
+        -- provider = function(self)
+            -- -- 0 is just another output, we can decide to print it or not!
+            -- return self.errors > 0 and (self.error_icon .. self.errors .. " ")
+        -- end,
+        -- hl = { fg = colors.diag.error },
+    -- },
+    -- {
+        -- provider = function(self)
+            -- return self.warnings > 0 and (self.warn_icon .. self.warnings .. " ")
+        -- end,
+        -- hl = { fg = colors.diag.warn },
+    -- },
+    -- {
+        -- provider = function(self)
+            -- return self.info > 0 and (self.info_icon .. self.info .. " ")
+        -- end,
+        -- hl = { fg = colors.diag.info },
+    -- },
+    -- {
+        -- provider = function(self)
+            -- return self.hints > 0 and (self.hint_icon .. self.hints)
+        -- end,
+        -- hl = { fg = colors.diag.hint },
+    -- },
+    -- {
+        -- provider = "]",
+    -- },
+-- }
 
 local Git = {
     condition = conditions.is_git_repo,
@@ -319,7 +319,7 @@ local Align = { provider = "%=" }
 local Space = { provider = " " }
 
 local DefaultStatusline = {
-    ViMode, Space, FileName, Space, Space, Diagnostics, Align,
+    ViMode, Space, FileName, Space, Space, Align,
     Space, Space, Space, Space, Git, Ruler, Space,
 }
 
