7743227a45f930a029dde3301169438a83b97a50
RoryNesbitt <ranesbitt@live.co.uk>
2022-04-11 11:02:22 +0100

    Removed the check that colorscheme succeeded because it does but it returns false

--- lua/colourscheme.lua
+++ lua/colourscheme.lua
@@ -2,8 +2,8 @@ local colorscheme = "base16-gruvbox-dark-hard"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
-	vim.notify("Colorscheme: " .. colorscheme .. " not found.")
-	return
+	-- vim.notify("Colorscheme: " .. colorscheme .. " not found.")
+	-- return
 end
 
 vim.cmd([[
