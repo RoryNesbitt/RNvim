3007f5e9b3c5ae0fbc63461edfbb84c9cd0378dd
RoryNesbitt <ranesbitt@live.co.uk>
2022-07-28 16:57:53 +0100

    Added git hydra from the examples

--- lua/rnvim/hydra.lua
+++ lua/rnvim/hydra.lua
@@ -106,3 +106,68 @@ hydra({
       { "<Esc>", nil, { exit = true, nowait = true } },
    }
 })
+
+local hint = [[
+ _J_: next hunk   _s_: stage hunk        _d_: show deleted   _b_: blame line
+ _K_: prev hunk   _u_: undo last stage   _p_: preview hunk   _B_: blame show full 
+ ^ ^              _S_: stage buffer      ^ ^                 _/_: show base file
+ ^
+ ^ ^              _<Enter>_: Neogit              _q_: exit
+]]
+
+hydra({
+   name = 'Git',
+   hint = hint,
+   config = {
+      buffer = bufnr,
+      color = 'pink',
+      invoke_on_body = true,
+      hint = {
+         border = 'rounded'
+      },
+      on_enter = function()
+         vim.cmd 'mkview'
+         vim.cmd 'silent! %foldopen!'
+         vim.bo.modifiable = false
+         require("gitsigns").toggle_signs(true)
+         require("gitsigns").toggle_linehl(true)
+      end,
+      on_exit = function()
+         local cursor_pos = vim.api.nvim_win_get_cursor(0)
+         vim.cmd 'loadview'
+         vim.api.nvim_win_set_cursor(0, cursor_pos)
+         vim.cmd 'normal zv'
+         require("gitsigns").toggle_signs(false)
+         require("gitsigns").toggle_linehl(false)
+         require("gitsigns").toggle_deleted(false)
+      end,
+   },
+   mode = {'n','x'},
+   body = '<leader>g',
+   heads = {
+      { 'J',
+         function()
+            if vim.wo.diff then return ']c' end
+            vim.schedule(function() require("gitsigns").next_hunk() end)
+            return '<Ignore>'
+         end,
+         { expr = true, desc = 'next hunk' } },
+      { 'K',
+         function()
+            if vim.wo.diff then return '[c' end
+            vim.schedule(function() require("gitsigns").prev_hunk() end)
+            return '<Ignore>'
+         end,
+         { expr = true, desc = 'prev hunk' } },
+      { 's', ':Gitsigns stage_hunk<CR>', { silent = true, desc = 'stage hunk' } },
+      { 'u', require("gitsigns").undo_stage_hunk, { desc = 'undo last stage' } },
+      { 'S', require("gitsigns").stage_buffer, { desc = 'stage buffer' } },
+      { 'p', require("gitsigns").preview_hunk, { desc = 'preview hunk' } },
+      { 'd', require("gitsigns").toggle_deleted, { nowait = true, desc = 'toggle deleted' } },
+      { 'b', require("gitsigns").blame_line, { desc = 'blame' } },
+      { 'B', function() require("gitsigns").blame_line{ full = true } end, { desc = 'blame show full' } },
+      { '/', require("gitsigns").show, { exit = true, desc = 'show base file' } }, -- show the base of the file
+      { '<Enter>', '<Cmd>Neogit<CR>', { exit = true, desc = 'Neogit' } },
+      { 'q', nil, { exit = true, nowait = true, desc = 'exit' } },
+   }
+})
