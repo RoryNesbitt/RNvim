3d9356df9605b3c9bebd97c95b2d6dae696e06a5
RoryNesbitt <ranesbitt@live.co.uk>
2022-08-03 13:22:57 +0100

    Moved firenvim config to lua

--- lua/rnvim/firenvim.lua
+++ lua/rnvim/firenvim.lua
@@ -1,27 +1,36 @@
-vim.cmd([[
-  let g:firenvim_config = {
-    \ 'globalSettings': {
-      \ 'alt': 'all',
-    \  },
-    \ 'localSettings': {
-      \ '.*': {
-        \ 'cmdline': 'neovim',
-        \ 'content': 'text',
-        \ 'priority': 0,
-        \ 'selector': 'textarea',
-        \ 'takeover': 'never',
-      \ },
-    \ }
-  \ }
+vim.g.firenvim_config = {
+  globalSettings = {
+    alt = 'all',
+  },
+  localSettings = {},
+}
 
-  if exists('g:started_by_firenvim')
-    set laststatus=0
-    set wrap
-    set linebreak
-    set showbreak=~
-    nnoremap j gj
-    nnoremap k gk
-    au BufEnter github.com_*.txt set filetype=markdown
-    au TextChanged,TextChangedI * ++nested write
-  endif
-]])
+local fc = vim.g.firenvim_config['localSettings']
+fc['.*'] = {
+  cmdline = 'neovim',
+  content = 'text',
+  priority = 0,
+  selector = 'textarea',
+  takeover = 'never',
+}
+
+if vim.g.started_by_firenvim then
+  vim.o.laststatus=0
+  vim.wo.wrap=true
+  vim.wo.linebreak=true
+  vim.o.showbreak='~'
+  vim.keymap.set("n", "j", "gj", {noremap = true})
+  vim.keymap.set("n", "k", "gk", {noremap = true})
+  local fireau = vim.api.nvim_create_augroup("start_by_fire", { clear = true })
+  vim.api.nvim_create_autocmd("bufEnter", {
+    pattern = "github.com_*.txt",
+    command = "set filetype=markdown",
+    group = fireau
+  })
+  vim.api.nvim_create_autocmd({"TextChanged", "TextChangedI"}, {
+    pattern = "*",
+    command = "write",
+    nested = true,
+    group = fireau
+  })
+end
