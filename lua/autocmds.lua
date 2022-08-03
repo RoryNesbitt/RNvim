8c4e64dc93779d951b94e225b5eaaeadf7fff63d
rory <ranesbitt@live.co.uk>
2022-08-03 16:39:02 +0100

    Put all the autocommands in the one group

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,36 +1,37 @@
 local confDir = vim.fn.stdpath('config')..'/lua/'
 
-local onWrite = vim.api.nvim_create_augroup("onFileWrite", { clear = true })
+local group = vim.api.nvim_create_augroup("RNvim", { clear = true })
+
 vim.api.nvim_create_autocmd( "BufWritePost", {
   pattern = "packer-init.lua",
   command = "source <afile> | PackerSync",
-  group = onWrite
+  group = group
 })
+
 vim.api.nvim_create_autocmd( "BufWritePost", {
   pattern = "tmux.conf",
   command = "!tmux source-file ~/.config/tmux/tmux.conf; tmux display-message 'Reloaded tmux config'",
-  group = onWrite
+  group = group
 })
 
-local onFileType vim.api.nvim_create_augroup("onFileType", { clear = true })
 vim.api.nvim_create_autocmd("FileType", {
   pattern = {"markdown", "tex", "text"},
   command = "setlocal textwidth=80",
-  group = onFileType
+  group = group
 })
+
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "*",
-  group = onFileType,
   callback = function ()
     if vim.bo.filetype ~= "dashboard" then
       vim.cmd([[match TrailingSpace /\s\+$/]])
     end
-  end
+  end,
+  group = group,
 })
 
-local lualineColors vim.api.nvim_create_augroup("lualineColors", { clear = true })
 vim.api.nvim_create_autocmd("ColorScheme", {
   pattern = "*",
   callback = "luafile "..confDir.."rnvim/lualine.lua",
-  group = lualineColors
+  group = group
 })
