67d0a4e8379365cbee731374bb0dbd5a208cfef1
RoryNesbitt <ranesbitt@live.co.uk>
2022-10-17 17:35:41 +0100

    Made pvim dashboard logos

--- lua/rnvim/dashboard/rnvim.lua
+++ lua/rnvim/dashboard/rnvim.lua
@@ -1,52 +1,4 @@
-local status_ok, db = pcall(require, "dashboard")
-if not status_ok then
-	return
-end
-
-vim.g.dashboard_default_executive = "telescope"
-
-local configDir = vim.fn.stdpath('config')
-local dataDir = vim.fn.stdpath('data').."/site"
-local dir = os.getenv("PVIM")
-if dir then
-  configDir = dir.."/config"
-  dataDir = dir
-end
-
-db.custom_center = {
-	{ icon = "  ", desc = "Jump to File                ", action = "Telescope find_files" },
-	{ icon = "  ", desc = "Recent Files                ", action = "Telescope oldfiles" },
-	{ icon = "  ", desc = "Open Neogit                 ", action = "Neogit" },
-	{ icon = "  ", desc = "Search Text                 ", action = "Telescope grep_string" },
-	{ icon = "  ", desc = "New File                    ", action = "DashboardNewFile" },
-	{ icon = "  ", desc = "Edit Config                 ", action = function ()
-    vim.cmd("cd"..configDir)
-    vim.cmd("Telescope find_files")
-  end},
-	{ icon = "  ", desc = "Update Config               ", action = function ()
-    print('Pulling config')
-    print(vim.fn.system('git --git-dir='..configDir..'/.git --work-tree='..configDir..' pull'))
-    dofile(configDir..'/lua/packer-init.lua')
-    vim.cmd('PackerSync')
-    print('Restart Neovim to see any changes')
-  end},
-  --   
-}
-
-local function pluginCount()
-	local i, popen = 0, io.popen
-	for _ in popen("ls "..dataDir.."/pack/packer/start/"):lines() do
-		i = i + 1
-	end
-	for _ in popen("ls "..dataDir.."/pack/packer/opt/"):lines() do
-		i = i + 1
-	end
-	return i
-end
-
-db.custom_footer = { "Neovim loaded with " .. pluginCount() .. " plugins" }
---
-local logos = {
+return {
   {
     [[]],
     [[]],
@@ -297,7 +249,3 @@ local logos = {
     [[]],
   },
 }
-
-math.randomseed(os.clock()*100000000000)
-local s = math.random(1,#logos)
-db.custom_header = logos[s]
