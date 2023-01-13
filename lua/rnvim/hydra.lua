be6bb392afe3dad9ff939fdc483b1f7b9c1aeece
RoryNesbitt <ranesbitt@live.co.uk>
2023-01-13 12:34:56 +0000

    Run through formatter

--- lua/rnvim/hydra.lua
+++ lua/rnvim/hydra.lua
@@ -1,7 +1,7 @@
 local hydra = require("hydra")
 
 local function cmd(command)
-   return table.concat({ "<cmd>", command, "<CR>" })
+  return table.concat({ "<cmd>", command, "<CR>" })
 end
 
 hydra({
@@ -19,7 +19,8 @@ hydra({
     { "N", function() require("dap").step_over() end, { desc = "Step over" } },
     { "O", function() require("dap").step_out() end, { desc = "Step out" } },
     { "b", function() require("dap").toggle_breakpoint() end, { desc = "Breakpoint" } },
-    { "B", function() require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: ")) end, { desc = "Conditional breakpoint" } },
+    { "B", function() require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: ")) end,
+      { desc = "Conditional breakpoint" } },
     { "L", function() require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
     end, { desc = "Log point" } },
     { "r", function() require("dap").repl.open() end, { desc = "Repl" } },
@@ -44,41 +45,41 @@ local hint = [[
 ]]
 
 hydra({
-   name = "Telescope",
-   hint = hint,
-   config = {
-      color = "teal",
-      invoke_on_body = true,
-      hint = {
-         position = "middle",
-         border = "rounded",
-      },
-   },
-   mode = "n",
-   body = "<Leader>t",
-   heads = {
-      { "f", cmd "Telescope filetypes" },
-      { "g", cmd "Telescope live_grep" },
-      { "o", cmd "Telescope oldfiles", { desc = "recently opened files" } },
-      { "h", cmd "Telescope help_tags", { desc = "vim help" } },
-      { "k", cmd "Telescope keymaps" },
-      { "r", cmd "Telescope resume" },
-      { "/", function()
-        require("telescope.builtin").current_buffer_fuzzy_find({
-          sorting_strategy = "ascending",
-          layout_config = { prompt_position = "top" },
-          previewer = false,
-        })
-      end , { desc = "search in file" } },
-      { "t", cmd "TodoTelescope" },
-      { "b", cmd "Telescope buffers" },
-      { "?", cmd "Telescope search_history",  { desc = "search history" } },
-      { ";", cmd "Telescope command_history", { desc = "command-line history" } },
-      { "c", cmd "Telescope commands", { desc = "execute command" } },
-      { "<Enter>", cmd "Telescope", { exit = true, desc = "list all pickers" } },
-      { "q", nil, { exit = true, nowait = true, desc = "exit" } },
-      { "<Esc>", nil, { exit = true, nowait = true } },
-   }
+  name = "Telescope",
+  hint = hint,
+  config = {
+    color = "teal",
+    invoke_on_body = true,
+    hint = {
+      position = "middle",
+      border = "rounded",
+    },
+  },
+  mode = "n",
+  body = "<Leader>t",
+  heads = {
+    { "f", cmd "Telescope filetypes" },
+    { "g", cmd "Telescope live_grep" },
+    { "o", cmd "Telescope oldfiles", { desc = "recently opened files" } },
+    { "h", cmd "Telescope help_tags", { desc = "vim help" } },
+    { "k", cmd "Telescope keymaps" },
+    { "r", cmd "Telescope resume" },
+    { "/", function()
+      require("telescope.builtin").current_buffer_fuzzy_find({
+        sorting_strategy = "ascending",
+        layout_config = { prompt_position = "top" },
+        previewer = false,
+      })
+    end, { desc = "search in file" } },
+    { "t", cmd "TodoTelescope" },
+    { "b", cmd "Telescope buffers" },
+    { "?", cmd "Telescope search_history", { desc = "search history" } },
+    { ";", cmd "Telescope command_history", { desc = "command-line history" } },
+    { "c", cmd "Telescope commands", { desc = "execute command" } },
+    { "<Enter>", cmd "Telescope", { exit = true, desc = "list all pickers" } },
+    { "q", nil, { exit = true, nowait = true, desc = "exit" } },
+    { "<Esc>", nil, { exit = true, nowait = true } },
+  }
 })
 
 local hint = [[
@@ -90,65 +91,65 @@ local hint = [[
 ]]
 
 hydra({
-   name = "Git",
-   hint = hint,
-   config = {
-      buffer = bufnr,
-      color = "pink",
-      invoke_on_body = true,
-      hint = {
-         border = "rounded"
-      },
-      on_enter = function()
-         if vim.bo.filetype ~= "dashboard" then
-           vim.cmd.mkview()
-           vim.cmd "silent! %foldopen!"
-           vim.bo.modifiable = false
-           require("gitsigns").toggle_linehl(true)
-         end
+  name = "Git",
+  hint = hint,
+  config = {
+    buffer = bufnr,
+    color = "pink",
+    invoke_on_body = true,
+    hint = {
+      border = "rounded"
+    },
+    on_enter = function()
+      if vim.bo.filetype ~= "dashboard" then
+        vim.cmd.mkview()
+        vim.cmd "silent! %foldopen!"
+        vim.bo.modifiable = false
+        require("gitsigns").toggle_linehl(true)
+      end
+    end,
+    on_exit = function()
+      if vim.bo.filetype ~= "dashboard" then
+        local cursor_pos = vim.api.nvim_win_get_cursor(0)
+        vim.cmd.loadview()
+        vim.api.nvim_win_set_cursor(0, cursor_pos)
+        vim.cmd.normal("zv")
+        require("gitsigns").toggle_linehl(false)
+        require("gitsigns").toggle_deleted(false)
+      end
+    end,
+  },
+  mode = { "n", "x" },
+  body = "<leader>g",
+  heads = {
+    { "n",
+      function()
+        if vim.wo.diff then return "]c" end
+        vim.schedule(function() require("gitsigns").next_hunk() end)
+        return "<Ignore>"
       end,
-      on_exit = function()
-         if vim.bo.filetype ~= "dashboard" then
-           local cursor_pos = vim.api.nvim_win_get_cursor(0)
-           vim.cmd.loadview()
-           vim.api.nvim_win_set_cursor(0, cursor_pos)
-           vim.cmd.normal("zv")
-           require("gitsigns").toggle_linehl(false)
-           require("gitsigns").toggle_deleted(false)
-         end
+      { expr = true, desc = "next hunk" } },
+    { "N",
+      function()
+        if vim.wo.diff then return "[c" end
+        vim.schedule(function() require("gitsigns").prev_hunk() end)
+        return "<Ignore>"
       end,
-   },
-   mode = {"n","x"},
-   body = "<leader>g",
-   heads = {
-      { "n",
-         function()
-            if vim.wo.diff then return "]c" end
-            vim.schedule(function() require("gitsigns").next_hunk() end)
-            return "<Ignore>"
-         end,
-         { expr = true, desc = "next hunk" } },
-      { "N",
-         function()
-            if vim.wo.diff then return "[c" end
-            vim.schedule(function() require("gitsigns").prev_hunk() end)
-            return "<Ignore>"
-         end,
-         { expr = true, desc = "prev hunk" } },
-      { "s", ":Gitsigns stage_hunk<CR>", { silent = true, desc = "stage hunk" } },
-      { "u", require("gitsigns").undo_stage_hunk, { desc = "undo last stage" } },
-      { "S", require("gitsigns").stage_buffer, { desc = "stage buffer" } },
-      { "p", require("gitsigns").preview_hunk, { desc = "preview hunk" } },
-      { "d", require("gitsigns").toggle_deleted, { nowait = true, desc = "toggle deleted" } },
-      { "b", require("gitsigns").blame_line, { desc = "blame" } },
-      { "B", function() require("gitsigns").blame_line{ full = true } end, { desc = "blame show full" } },
-      { "/", require("gitsigns").show, { exit = true, desc = "show base file" } }, -- show the base of the file
-      { "<Enter>", cmd "Neogit", { exit = true, desc = "Neogit" } },
-      { "c", cmd "Neogit commit", { exit = true, desc = "Commit" } },
-      { "g", cmd "Neogit", { exit = true } },
-      { "q", nil, { exit = true, nowait = true, desc = "exit" } },
-      { "<Esc>", nil, { exit = true, nowait = true } },
-   }
+      { expr = true, desc = "prev hunk" } },
+    { "s", ":Gitsigns stage_hunk<CR>", { silent = true, desc = "stage hunk" } },
+    { "u", require("gitsigns").undo_stage_hunk, { desc = "undo last stage" } },
+    { "S", require("gitsigns").stage_buffer, { desc = "stage buffer" } },
+    { "p", require("gitsigns").preview_hunk, { desc = "preview hunk" } },
+    { "d", require("gitsigns").toggle_deleted, { nowait = true, desc = "toggle deleted" } },
+    { "b", require("gitsigns").blame_line, { desc = "blame" } },
+    { "B", function() require("gitsigns").blame_line { full = true } end, { desc = "blame show full" } },
+    { "/", require("gitsigns").show, { exit = true, desc = "show base file" } }, -- show the base of the file
+    { "<Enter>", cmd "Neogit", { exit = true, desc = "Neogit" } },
+    { "c", cmd "Neogit commit", { exit = true, desc = "Commit" } },
+    { "g", cmd "Neogit", { exit = true } },
+    { "q", nil, { exit = true, nowait = true, desc = "exit" } },
+    { "<Esc>", nil, { exit = true, nowait = true } },
+  }
 })
 
 
@@ -170,5 +171,3 @@ hydra({
     { "q", nil, { exit = true, nowait = true, desc = "exit" } },
   }
 })
-
-
