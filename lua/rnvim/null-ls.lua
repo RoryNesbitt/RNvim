84b8bb92c73b093732020929752b87715c7367c4
rory <ranesbitt@live.co.uk>
2023-04-09 13:39:54 +0100

    Fixed for plugin change

--- lua/rnvim/null-ls.lua
+++ lua/rnvim/null-ls.lua
@@ -1,34 +1,36 @@
 local status_ok, null_ls = pcall(require, "null-ls")
 if not status_ok then return end
 
-local mason_null_ls = require("mason-null-ls")
-
-mason_null_ls.setup { automatic_setup = true }
-
 null_ls.setup {
   sources = {
     -- Anything not supported by mason goes here
   }
 }
 
-mason_null_ls.setup_handlers {
-  function(source_name, methods)
-    -- All sources with no handler get passed here
-    require("mason-null-ls.automatic_setup")(source_name, methods)
-  end,
+local status_ok, mason_null_ls = pcall(require, "mason-null-ls")
+if not status_ok then return end
 
-  write_good = function(source_name, methods)
-    null_ls.register(null_ls.builtins.diagnostics.write_good.with {
-      filetypes = { "markdown", "tex" }
-    })
-  end,
+mason_null_ls.setup {
+  automatic_setup = true,
+  handlers = {
+    function(source_name, methods)
+      -- All sources with no handler get passed here
+      require("mason-null-ls.automatic_setup")(source_name, methods)
+    end,
 
-  lua_format = function(source_name, methods)
-    null_ls.register(null_ls.builtins.formatting.lua_format.with {
-      extra_args = {
-        "--no-keep-simple-function-one-line", "--no-break-after-operator",
-        "--column-limit=80", "--break-after-table-lb", "--indent-width=2"
-      }
-    })
-  end
+    write_good = function(source_name, methods)
+      null_ls.register(null_ls.builtins.diagnostics.write_good.with {
+        filetypes = { "markdown", "tex" }
+      })
+    end,
+
+    lua_format = function(source_name, methods)
+      null_ls.register(null_ls.builtins.formatting.lua_format.with {
+        extra_args = {
+          "--no-keep-simple-function-one-line", "--no-break-after-operator",
+          "--column-limit=80", "--break-after-table-lb", "--indent-width=2"
+        }
+      })
+    end
+  }
 }
