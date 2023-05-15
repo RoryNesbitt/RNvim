0f9366378b61905f2316a8692acf66ee2f4bb1f5
rory <ranesbitt@live.co.uk>
2023-05-15 18:44:20 +0100

    q to quit autocmd from lazyvim

--- lua/rnvim/autocmds.lua
+++ lua/rnvim/autocmds.lua
@@ -51,3 +51,26 @@ vim.api.nvim_create_autocmd("ColorScheme", {
   end,
   group = group
 })
+
+-- close some filetypes with <q>
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = {
+    "netrw",
+    "PlenaryTestPopup",
+    "help",
+    "lspinfo",
+    "man",
+    "notify",
+    "qf",
+    "spectre_panel",
+    "startuptime",
+    "tsplayground",
+    "checkhealth",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+  end,
+  group = group
+})
+
