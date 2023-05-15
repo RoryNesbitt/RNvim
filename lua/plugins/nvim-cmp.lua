484d4e66bf5651d0b3f22529ede84dfbe2c78b3a
rory <ranesbitt@live.co.uk>
2023-05-15 19:36:54 +0100

    Updated cmp settings

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -27,13 +27,14 @@ cmp.setup {
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping.abort(),
-    ["<CR>"] = cmp.mapping.confirm { select = true }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ["<CR>"] = cmp.mapping.confirm { select = false }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
   },
   sources = cmp.config.sources {
     { name = "nvim_lsp" },
     { name = "luasnip" },
-  }, {
     { name = "buffer" },
+    { name = "path" },
+  }, {
   },
   formatting = {
     format = lspkind.cmp_format { with_text = true, maxwidth = 50 },
