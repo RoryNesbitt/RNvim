e4cc43b62afa40d220b84a2175adef2abf4a3e92
rory <ranesbitt@live.co.uk>
2023-05-15 19:11:10 +0100

    Added nvim-notify

--- lua/rnvim/lazy/plugins.lua
+++ lua/rnvim/lazy/plugins.lua
@@ -196,6 +196,21 @@ return {
   },
   --Functionality
   {
+    "rcarriga/nvim-notify",
+    opts = {
+      timeout = 3000,
+      max_height = function()
+        return math.floor(vim.o.lines * 0.75)
+      end,
+      max_width = function()
+        return math.floor(vim.o.columns * 0.75)
+      end,
+    },
+    init = function()
+      vim.notify = require("notify")
+    end,
+  },
+  {
     "anuvyklack/hydra.nvim",
     config = function() require("plugins.hydra") end
   },
