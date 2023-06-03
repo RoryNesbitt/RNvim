1998e3c05ee4ad94d63d5825daab6e966ddc6117
Rory <ranesbitt@live.co.uk>
2023-06-03 22:25:07 +0100

    chore(plugins): consolidate plugins into one file

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -301,4 +301,23 @@ return {
     "aserowy/tmux.nvim",
     config = function() require("configs.tmux") end,
   },
+  {
+    "zbirenbaum/copilot-cmp",
+    dependencies = "copilot.lua",
+    config = function ()
+      local copilot_cmp = require("copilot_cmp")
+      copilot_cmp.setup()
+    end,
+    dependencies = {
+      "hrsh7th/nvim-cmp",
+      {
+        "zbirenbaum/copilot.lua",
+        cmd = "Copilot",
+        opts = {
+          suggestion = { enabled = false },
+          panel = { enabled = false },
+        },
+      }
+    },
+  },
 }
