5330abb46e17fa59be76c48ebf8d0f56bc9f05c0
Rory <ranesbitt@live.co.uk>
2023-08-07 18:33:49 +0100

    fix(cmp): get command line completion again

--- lua/configs/nvim-cmp.lua
+++ lua/configs/nvim-cmp.lua
@@ -66,9 +66,8 @@ cmp.setup.cmdline("/", {
 cmp.setup.cmdline(":", {
   mapping = cmp.mapping.preset.cmdline(),
   sources = cmp.config.sources {
-    { name = "path" }
-  }, {
-    { name = "cmdline" }
+    { name = "path" },
+    { name = "cmdline" },
   }
 })
 
